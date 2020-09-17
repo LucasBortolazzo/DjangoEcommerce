b5aaeebe3e0ab5e21d6b4681cc7e6248f49b0c0e
lucas-bd <lucas.dolberth@yahoo.com>
2020-09-17 20:11:54 -0300

    added app catalog

---
+++ catalog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Category(models.Models):
+    name = models.CharField('Nome', max_length=100)
+    slug = models.SlugField('Identificador', max_length=100)
+
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plutal = 'Categorias'
+        ordering = ['name']
+
+class Product(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    slug = models.SlugField('Identificador', max_length=100)
+    category = models.ForeignKey('catalog.Category', verbose_name='Categoria')
+    description = models.TextField('Descrição', blank=True)
+    price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+
+
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)            
+    
+
--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -129,3 +129,8 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass    
