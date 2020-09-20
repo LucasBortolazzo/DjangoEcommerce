536b2ba4097c71bb219c0f8464553607d6ec1303
lucas-bd <lucas.dolberth@yahoo.com>
2020-09-20 11:00:26 -0300

    added tests for ContactForm

--- core/forms.py
+++ core/forms.py
@@ -4,3 +4,12 @@ class ContactForm(forms.Form):
     name = forms.CharField(label='Nome', required=True)
     email = forms.EmailField(label='E-mail')
     message = forms.CharField(label='Mensagem', widget=forms.Textarea)
+
+"""
+Para renderizar o form manualmente
+        def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['message'].widget.attrs['class'] = 'form-control' 
+"""        
--- core/tests/test_view.py
+++ core/tests/test_view.py
@@ -16,6 +16,18 @@ class IndexViewTestCase(TestCase):
 
     def test_template_used(self):
         response = self.client.get(self.url)
-        self.assertTemplateUsed(response, 'index.html')          
+        self.assertTemplateUsed(response, 'index.html')   
+
+class ContactViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('contact')
+
+    def TearDown(self):
+        pass
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)          
 
 
