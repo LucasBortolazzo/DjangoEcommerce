f1a9b9635430fe8287942c3c6d27af18023ca728
lucas-bd <lucas.dolberth@yahoo.com>
2020-09-20 10:08:13 -0300

    using django forms in project

--- core/views.py
+++ core/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from catalog.models import Category
+from .forms import ContactForm
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    form = ContactForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'contact.html', context)
 
 def product(request):
     return render(request, 'product.html')       
