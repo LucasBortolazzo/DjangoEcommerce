adb42f72325e2bebccacbd404330ceb9d6a05c46
Lucas Bortolazzo <lucas.dolberth@yahoo.com>
2020-09-27 08:08:10 -0300

    add class based views

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 from .models import Product, Category
 from django.db import connection
 
+
+class ProductListView(generic.ListView):
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
+    queryset = Product.objects.all()
+    paginate_by = 2
+    #model = Product
+
+
+product_list = ProductListView.as_view()    
+        
+
+'''
 def product_list(request):
     context = {
         'product_list': Product.objects.all()
     }
 
     return render(request, 'catalog/product_list.html', context)   
+'''
+class CategoryListView(generic.ListView):
 
+    template_name = 'catalog/category.html' 
+    context_object_name = 'product_list'
+
+    def get_queryset(self):
+        return Product.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs) 
+        context['current_category'] = get_object_or_404(Category, slug=self.kwargs['slug']) 
+        return context       
+
+category = CategoryListView.as_view()
+            
+
+
+'''
 def category(request, slug):
     categoria = Category.objects.get(slug=slug)
 
@@ -19,7 +51,9 @@ def category(request, slug):
        'current_category': categoria.name,
        'product_list': Product.objects.filter(category=categoria.id) 
     }
-    return render(request, 'catalog/category.html', context)      
+    return render(request, 'catalog/category.html', context)    
+
+'''      
   
 def productdetail(request, slug):
     produto = Product.objects.get(slug=slug)
@@ -28,4 +62,4 @@ def productdetail(request, slug):
         'product': produto
     }  
 
-    return render(request, 'catalog/product.html', context)
+    return render(request, 'catalog/product.html', context)   
